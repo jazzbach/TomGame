8e16f916b3661d80c07ed3a1b4705eda97bf91bc
jazzbach <chilljazzbach@gmail.com>
2019-04-06 09:57:50 -0600

    added fb button

--- app/src/main/java/com/vfs/tom/MainActivity.java
+++ app/src/main/java/com/vfs/tom/MainActivity.java
@@ -1,5 +1,6 @@
 package com.vfs.tom;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -8,9 +9,18 @@ import android.view.View;
 import android.widget.ImageButton;
 import android.widget.TextView;
 
+import com.facebook.AccessToken;
+import com.facebook.CallbackManager;
+import com.facebook.FacebookCallback;
+import com.facebook.FacebookException;
+import com.facebook.login.LoginManager;
+import com.facebook.login.LoginResult;
+import com.facebook.login.widget.LoginButton;
 import com.vfs.tom.managers.PersistenceManager;
 import com.vfs.tom.model.Enemy;
 import com.vfs.tom.model.Player;
+
+import java.util.Arrays;
 
 public class MainActivity extends AppCompatActivity {
     //TODO generate player class (damage done, current weapon, ?)
@@ -24,6 +34,12 @@ public class MainActivity extends AppCompatActivity {
     private TextView enemyName;
     private TextView tempEnergy;
 
+    //FB
+    private CallbackManager callbackManager = CallbackManager.Factory.create();
+    private static final String EMAIL = "email";
+    private LoginButton loginButton;
+    //FBEND
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -31,12 +47,74 @@ public class MainActivity extends AppCompatActivity {
         initView();
         setUpGame();
         setListeners();
+
+        //FB
+        LoginManager.getInstance().registerCallback(callbackManager,
+                new FacebookCallback<LoginResult>() {
+                    @Override
+                    public void onSuccess(LoginResult loginResult) {
+                        // App code
+                        Log.e("yameloguee","jiji");
+                    }
+
+                    @Override
+                    public void onCancel() {
+                        Log.e("yameloguee","no");
+                        // App code
+                    }
+
+                    @Override
+                    public void onError(FacebookException exception) {
+                        Log.e("yameloguee","error");
+                        Log.e("yameloguee",exception.getStackTrace().toString());
+                        exception.printStackTrace();
+
+                        // App code
+                    }
+                });
+        //FBEND
+        //FB
+        AccessToken accessToken = AccessToken.getCurrentAccessToken();
+        boolean isLoggedIn = accessToken != null && !accessToken.isExpired();
+        //FBEND
+
     }
+
+    //FB
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        callbackManager.onActivityResult(requestCode, resultCode, data);
+        super.onActivityResult(requestCode, resultCode, data);
+    }
+    //Every activity and fragment that you integrate with the FacebookSDK Login or Share should forward onActivityResult to the callbackManager.
+    //FBEND
 
     protected void initView(){
         enemyBody = findViewById(R.id.enemy_body);
         enemyName = findViewById(R.id.enemy_name);
         tempEnergy = findViewById(R.id.temp_energy);
+        //FB
+        loginButton = (LoginButton) findViewById(R.id.login_button);
+        loginButton.setReadPermissions("email", "public_profile", "user_friends");
+        // If you are using in a fragment, call loginButton.setFragment(this);
+        // Callback registration
+        loginButton.registerCallback(callbackManager, new FacebookCallback<LoginResult>() {
+            @Override
+            public void onSuccess(LoginResult loginResult) {
+                // App code
+            }
+
+            @Override
+            public void onCancel() {
+                // App code
+            }
+
+            @Override
+            public void onError(FacebookException exception) {
+                // App code
+            }
+        });
+        //FBEND
     }
     private void setListeners(){
         enemyBody.setOnTouchListener(new View.OnTouchListener() {
